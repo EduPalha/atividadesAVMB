0ca63dc340f090ec50ad65e13a2b6c13fc83e4f8
Eduardo Palharini <eduardo.palharini@avmb.com.br>
2022-11-14 18:07:23 -0300

    atividades 4

---
+++ estruturaVisualStudio/Main com SWITCH.cs
@@ -0,0 +1,61 @@
+﻿namespace Aula2010
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomearquivo;
+            int op;
+            Arquivo a;
+            Console.WriteLine("Digite o nome do arquivo: ");
+            nomearquivo = Console.ReadLine();
+            a = new Arquivo(nomearquivo);
+            while (true)
+            {
+                Console.WriteLine("Digite:\n1 - para criar/abrir um arquivo\n" +
+                    "2 - para gravar um texto no arquivo\n" +
+                    "3 - para ler o arquivo\n" +
+                    "0 - para sair do programa");
+                op = int.Parse(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                        a.criaAbreArquivo();
+                        a.fechaArquivo();
+                        break;
+                    case 2:
+                        a.criaAbreArquivo();
+                        Console.WriteLine("Digite uma mensagem para o futuro: ");
+                        string msg = Console.ReadLine();
+                        a.gravaMensagem(msg);
+                        a.fechaArquivo();
+                        break;
+                    case 3:
+                        a.lerArquivo();
+                        break;
+                    case 0:
+                        break;
+
+                    default:
+                        Console.WriteLine("digite um valor valido animalzinho s2");
+                        break;
+                }
+            }
+
+
+         /*
+            a.gravaMensagem("Oi gente, vocês são 10!");
+           // a.fechaArquivo();
+
+           // a.criaAbreArquivo();
+            a.gravaMensagem("Mas nao tenho certeza");
+            a.gravaMensagem("pode sim gente, vocês são 100");
+            a.gravaMensagem("e aqui posso escrever muito!");
+            //a.fechaArquivo();*/
+            
+        
+
+        }
+
+    }
+}
--- estruturaVisualStudio/Program.cs
+++ estruturaVisualStudio/Program.cs
@@ -1,18 +1,56 @@
 ﻿/* Criando, consultando, editando arquivos */
 
+using System;
 using System.IO;
 
 namespace ImersaoAVMB
 {
     class Program
     {
-        
+
         static void Main(string[] args)
         {
-            StreamWriter sw = new StreamWriter("C:\\Users\\eduardo.palharini\\Desktop\\atividadesAVMB\\estruturaVisualStudio\\emails.txt");
+            int opcao = 0;
+            string arquivo = "C:\\Users\\eduardo.palharini\\Desktop\\atividadesAVMB\\estruturaVisualStudio\\emails.txt";
+            StreamWriter sw = new StreamWriter(arquivo, true);
+         
+            StreamReader sr;
             try
-            {  
-                sw.WriteLine("Olá;");
+            {
+                while (true)
+                {
+                    if (opcao == 3){
+                        break;
+                    }
+                    Console.WriteLine("--------Menu--------");
+                    Console.WriteLine("1 - Cadastrar email");
+                    Console.WriteLine("2 - Listar emails");
+                    Console.WriteLine("3 - Sair");
+                    Console.Write("Digite sua opcao: ");
+                    opcao = int.Parse(Console.ReadLine() + "");
+
+                    switch (opcao)
+                    {
+                        case 1:
+                            Console.Write("Email: ");
+                            sw.WriteLine(Console.ReadLine() + "");
+                            break;
+
+                        case 2:
+                            Console.WriteLine("\nEmails armazenados: ");
+                            sr = new StreamReader(arquivo);
+                            string linha = sr.ReadLine();
+                            while(linha != null){
+                                Console.WriteLine(linha);
+                                sr.Close();
+                            }
+                            Console.WriteLine("\n____________________");
+                            break;
+
+                        case 3:
+                            break;
+                    }
+                }
             }
             catch (Exception e)
             {
